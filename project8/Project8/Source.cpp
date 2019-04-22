80ae5ba90241812c312d36f689cca4de19ffb20b
kyle <thutzr1019@gmail.com>
2019-04-22 23:38:31 +0800

    add project8

--- project8/Project8/Source.cpp
+++ project8/Project8/Source.cpp
@@ -1,44 +1,60 @@
 #include <iostream>
-#include "BST.h"
+//add <>
+#include <stdlib.h>
 
 using namespace std;
 
 int* input;
-
+// 节点类型
 typedef struct BSTNode {
-	BSTNode *lchild;
-	BSTNode *rchild;
-	int data;
-}BSTNode;
-
+	BSTNode *lchild;//左孩子
+	BSTNode *rchild;//右孩子
+	int data;//变量值
+}BSTNode, *BST;
+//创建新节点
 BSTNode *createNode() {
 	BST node = (BST)malloc(sizeof(BSTNode));
 	if (node) {
-		node->lchild = node->rchild = NULL;
+		node->lchild = node->rchild = NULL;//左右孩子赋值为NULL
 		return node;
 	}
-	else {
+	else {//输入错误则退出
 		exit(0);
 	}
 }
-
-//void createTree(BSTNode*& root, int next) {
-//	if (input[next] < root->data) {
-//		BSTNode* p = createNode();
-//		p->data = input[next];
-//		root->lchild = p;
-//		createTree(p, next + 1);
-//	}
-//	else if (input[next] > root->data) {
-//		BSTNode* p = createNode();
-//		p->data = input[next];
-//		root->rchild = p;
-//		createTree(p, next + 1);
-//	}
-//
-//}
-
-int commonAncestor(BSTNode* root, int num1, int num2) {
+//插入节点
+void insert(BSTNode*& root, int num) {
+	BSTNode* temp = root;//代替根节点
+	BSTNode* preTemp = NULL;//之后将作为temp前面的一个节点，即temp的父节点
+	while (temp != NULL) {
+		preTemp = temp;
+		if ((temp->data) > num) {
+			temp = temp->lchild;
+		}
+		else {
+			temp = temp->rchild;
+		}
+	}
+	if (preTemp != NULL) {
+		BSTNode* leaf = createNode();
+		leaf->data = num;
+		if ((preTemp->data) > num) {
+			preTemp->lchild = leaf;
+		}
+		else {
+			preTemp->rchild = leaf;
+		}
+	}
+}
+//建立搜索树
+//add N
+void createTree(BSTNode*& root, int N) {
+	for (int i = 1; i < N; i++) {
+		insert(root, input[i]);
+	}
+}
+// 找到公共祖先
+int commonAncestor(BSTNode*& root, int num1, int num2) {
 	if ((num1 < root->data) && (num2 < root->data)) {
 		return commonAncestor(root->lchild, num1, num2);
 	}
@@ -47,27 +63,28 @@ int commonAncestor(BSTNode* root, int num1, int num2) {
 	}
 	return root->data;
 }
-
 int main() {
+	//输入数据
 	int N, M;
 	cin >> N;
 	cin >> M;
 	input = new int[N];
-	BST<int> bst;
 	for (int i = 0; i < N; i++) {
 		cin >> input[i];
-		bst.insert(input[i]);
 	}
-	int num1, num2, result;
-	BSTNode* root = createNode();
-	root->data = input[0];
-	
-
+	int NUM11, NUM21, result;
+	//建立根节点
+	BSTNode* root1 = createNode();
+	root1->data = input[0];
+	createTree(root1, N);
 	int* finalResult = new int[M];
 	for (int j = 0; j < M; j++) {
-		cin >> num1;
-		cin >> num2;
-		result = commonAncestor(root, num1, num2);
+		cin >> NUM11;
+		cin >> NUM21;
+		/*BSTNode* root1 = createNode();
+		root1->data = input[0];
+		createTree(root1);*/
+		result = commonAncestor(root1, NUM11, NUM21);
 		finalResult[j] = result;
 	}
 	for (int k = 0; k < M; k++) {
